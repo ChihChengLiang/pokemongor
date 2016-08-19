fd5a00791d5f6d08df0f6a8d1711892e459da9e9
Chih-Cheng Liang <chihchengliang@gmail.com>
2016-08-19 15:23:22 +0800

    batched

---
+++ R/pokemon_names.R
@@ -0,0 +1,13 @@
+#' Pokedex Names.
+#'
+#' Pokedex index and names of the first generation 151 pokemons.
+#'
+#' @source \url{https://gist.github.com/noxwyll/8c821408b1d943c3e500d91d77266368/f264e42f2e83bbe845841717f74b276c9801ba7d}, downloaded 2016-08-19
+#' @format Data frame with columns
+#' \describe{
+#'  \item{index}{Integer Pokedex index of the pokemon}
+#'  \item{name}{Proper name of pokemon}
+#' }
+#' @examples
+#' pokemon_names
+"pokemon_names"
--- data-raw/pokemons.R
+++ data-raw/pokemons.R
@@ -2,10 +2,10 @@ library(magrittr)
 library(dplyr)
 
 gist_url <- "https://gist.githubusercontent.com/noxwyll/8c821408b1d943c3e500d91d77266368/raw/f264e42f2e83bbe845841717f74b276c9801ba7d/GAME_MASTER_POKEMON_v0_2.tsv"
-pokemon_df <- read.table(gist_url, sep = "\t", header = T, stringsAsFactors = F)
+pokemon_raw <- read.table(gist_url, sep = "\t", header = T, stringsAsFactors = F)
 
 fix_decimal_marks <- function(vector) vector %>% gsub(",", ".", .) %>% as.numeric()
-pokemon_df_clean <- pokemon_df %>%
+pokemons <- pokemon_raw %>%
   mutate_each(
     funs(fix_decimal_marks),
     BaseCaptureRate,
@@ -17,8 +17,9 @@ pokemon_df_clean <- pokemon_df %>%
     HeightStdDev, WeightStdDev
   )
 
-pokemon_names <- pokemon_df_clean %>% select(index=PkMn, name=Identifier)
+pokemon_names <- pokemons %>% select(index=PkMn, name=Identifier)
 
-pokemon_df %>% write.csv("data-raw/pokemons.csv", row.names = F)
-pokemon_df_clean %>% save(file="data/pokemons.rda")
-pokemon_names %>% save(file="data/pokemon_names.rda")
+pokemons %>% write.csv("data-raw/pokemons.csv", row.names = F)
+
+devtools::use_data(pokemon_names, overwrite = TRUE)
+devtools::use_data(pokemons, overwrite = TRUE)
