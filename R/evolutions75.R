f534ceb29fe444cbf1e9b81b2be45f5960635ebd
Chih-Cheng Liang <chihchengliang@gmail.com>
2016-08-21 12:16:40 +0800

    fix pokemon's e'

--- R/evolutions75.R
+++ R/evolutions75.R
@@ -1,22 +1,22 @@
 #' OpenIntro 75 pokemons and their evolutions.
 #'
-#' This data set covers 75 Pokémon evolutions spread across
+#' This data set covers 75 Pokemon evolutions spread across
 #' four species. A wide set of variables are provided,
 #' allowing a deeper dive into what characteristics are
-#' important in predicting a Pokémon's final combat power (CP).
+#' important in predicting a Pokemon's final combat power (CP).
 #'
 #' @source \url{https://www.openintro.org/stat/data/?data=pokemon}, downloaded 2016-08-21
 #' @format Data frame with columns
 #' \describe{
-#'  \item{name}{A unique name given to the Pokémon}
-#'  \item{species}{The Pokémon's type, e.g. Pidgey.}
-#'  \item{cp}{Pre-evolution Combat Power, which is a summary of the Pokémon's strength for battling prior to the evolution of the Pokémon.}
-#'  \item{hp}{Pre-evolution Hit Points, which is a summary of how difficult it is to weaken the Pokémon in a battle.}
+#'  \item{name}{A unique name given to the Pokemon}
+#'  \item{species}{The Pokemon's type, e.g. Pidgey.}
+#'  \item{cp}{Pre-evolution Combat Power, which is a summary of the Pokemon's strength for battling prior to the evolution of the Pokemon.}
+#'  \item{hp}{Pre-evolution Hit Points, which is a summary of how difficult it is to weaken the Pokemon in a battle.}
 #'  \item{weight}{Pre-evolution weight, in kilograms.}
 #'  \item{height}{Pre-evolution height, in meters.}
-#'  \item{power_up_stardust}{Pre-evolution stardust required to power up the Pokémon.}
-#'  \item{power_up_candy}{Pre-evolution candy required to power up the Pokémon.}
-#'  \item{attack_weak}{The name of the pre-evolution weaker attack of the Pokémon.}
+#'  \item{power_up_stardust}{Pre-evolution stardust required to power up the Pokemon.}
+#'  \item{power_up_candy}{Pre-evolution candy required to power up the Pokemon.}
+#'  \item{attack_weak}{The name of the pre-evolution weaker attack of the Pokemon.}
 #'  \item{attack_weak_type}{The type of the pre-evolution weaker attack.}
 #'  \item{attack_weak_value}{The damage done by the pre-evolution weaker attack.}
 #'  \item{attack_strong}{The name of the pre-evolution stronger attack.}
@@ -26,8 +26,8 @@
 #'  \item{hp_new}{Post-evolution Hit Points.}
 #'  \item{weight_new}{Post-evolution weight, in kilograms.}
 #'  \item{height_new}{Post-evolution height, in meters.}
-#'  \item{power_up_stardust_new}{Post-evolution stardust required to power up the Pokémon.}
-#'  \item{power_up_candy_new}{Post-evolution candy required to power up the Pokémon.}
+#'  \item{power_up_stardust_new}{Post-evolution stardust required to power up the Pokemon.}
+#'  \item{power_up_candy_new}{Post-evolution candy required to power up the Pokemon.}
 #'  \item{attack_weak_new}{The name of the post-evolution weaker attack.}
 #'  \item{attack_weak_type_new}{The type of the post-evolution weaker attack.}
 #'  \item{attack_weak_value_new}{The damage done by the post-evolution weaker attack.}
